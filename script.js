a727cc4f4a85ac0c707b0fe5bcf9d617231d30ac
Yash Kamaat <yashtkamaat@gmail.com>
2021-10-03 10:54:17 -0400

    Created script.js and added exercise answers 1a through 2d

---
+++ script.js
@@ -0,0 +1,68 @@
+// SpreadDestructuringExercise
+// 1a
+const mcuShows = [
+"loki", 
+"what if...?"
+];
+console.log(mcuShows);
+// ================
+// 1b
+const starWarShows = [ 
+    "The Mandalorian",
+    "The Bad Batch"
+];
+// ================
+// 1c
+const disnerPlusShows = [
+...mcuShows,
+...starWarsShows
+];
+// ================
+// 1d
+console.log(disneyPlusShows);
+// ================
+// 2a
+const netflixMovies = {
+    action: "Extraction",
+    crime: "The Irishman"
+};
+// ================
+// 2b
+const amazonPrimeMovies = {
+action: "The Tomorrow War",
+drama: "One Night in Miami"
+};
+// ================
+// 2c
+const streamingMovies = {
+...amazonPrimeMovies,
+...netflixMovies,
+musical: "Hamilton",
+};
+// ================
+// 2d
+console.log(streamingMovies);
+// ================
+// 3a
+const
+// ================
+// 3b
+// ================
+// 3c
+// ================
+// 4a
+// ================
+// 4b
+// ================
+// 4c
+// ================
+// 4d
+// ================
+// 4e
+// ================
+// 4f
+// ================
+// 5a
+// ================
+// 5b
+// ================

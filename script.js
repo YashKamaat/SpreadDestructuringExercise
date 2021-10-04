d0638b402009acab820bde0b12dbd023b55195c5
Yash Kamaat <yashtkamaat@gmail.com>
2021-10-04 15:35:09 -0400

    lost work submitted again

--- script.js
+++ script.js
@@ -1,86 +1,91 @@
-// SpreadDestructuringExercise
 // 1a
-const mcuShows = [
-"loki", 
-"what if...?"
-];
-console.log(mcuShows);
-// ================
+const mcuShows = ["Loki", "What If...?"];
+//   =================
 // 1b
-const starWarShows = [ 
-    "The Mandalorian",
-    "The Bad Batch"
-];
-// ================
+const starWarsShows = ["The Mandalorian", "The Bad Batch"];
+//   =================
 // 1c
-const disnerPlusShows = [
-...mcuShows,
-...starWarsShows
-];
-// ================
+const disneyPlusShows = [...mcuShows, ...starWarsShows, "Monsters At Work"];
+//   =================
 // 1d
 console.log(disneyPlusShows);
-// ================
+//   =================
 // 2a
 const netflixMovies = {
-    action: "Extraction",
-    crime: "The Irishman"
+  action: "Extraction",
+  crime: "The Irishman",
 };
-// ================
+//  =================
 // 2b
 const amazonPrimeMovies = {
-action: "The Tomorrow War",
-drama: "One Night in Miami"
+  action: "The Tomorrow War",
+  drama: "One Night In Miami",
 };
-// ================
+// =================
 // 2c
 const streamingMovies = {
-...amazonPrimeMovies,
-...netflixMovies,
-musical: "Hamilton",
+  ...amazonPrimeMovies,
+  ...netflixMovies,
+  musical: "Hamilton",
 };
-// ================
+//  ================
 // 2d
 console.log(streamingMovies);
-// ================
+//  ================
 // 3a
 const disneyJunior = [
-    "Mickey Mouse Clubhouse",
-    "Spidey and His Amazing Friends"
+  "Mickey Mouse Clubhouse",
+  "Spidey and His Amazing Friends",
 ];
-// ================
+//   =================
 // 3b
-const [ mickey, spidey] = disneyJunior;
-// ================
+const [mickey, spidey] = disneyJunior;
+//   ================
 // 3c
-console.log(mickey,spidey);
-// ================
+console.log(disneyJunior);
+//   ================
 // 4a
 const avengers = {
-    warMacine: "James Rhodes",
-    theHulk: "Bruce Banner"
+  warMachine: "James Rhodes",
+  theHulk: "Bruce Banner",
 };
-// ================
+console.log(avengers);
+//   ================
 // 4b
-const {warMachine, theHulk} = avengers;
-// ================
+const { warMachine, theHulk } = avengers;
+//   console.log(avengers);
+//   =================
 // 4c
-console.log(warMachine, theHulk);
-// ================
+console.log(avengers);
+//   ================
 // 4d
 const moreAvengers = {
-    blackWidow: "Natasha Romanoff",
-    hawkeye: "Clint Barton",
-    ironMan: "Tony Stark"
+  blackWidow: "Natasha Romanoff",
+  hawkeye: "Clint Barton",
+  ironMan: "Tony Stark",
 };
-// ================
+//  ===============
 // 4e
-const { blackWidow: nat, ...others} = moreAvengers;
-// ================
+const { blackWidow: nat, ...others } = moreAvengers;
+console.log(moreAvengers);
+
 // 4f
 console.log(nat, others);
-// ================
+// ===============
+// BONUS
+// ===============
+const bonus = {
+  first: [1, 2, 3],
+  second: [4, 5, 6],
+  third: [7, 8, 9],
+};
+//  ==================
 // 5a
-// ================
-// 5b
-// ================
+// const all = [...bonus.first, ...bonus.second, ...bonus.third];
+// console.log(first);
+const all = bonus.first.concat(bonus.second, bonus.third);
+console.log(all);
+//   ==================
+//5b
+const [one, two, three, four, five, six, seven, eight, nine] = all;
+console.log(four);
